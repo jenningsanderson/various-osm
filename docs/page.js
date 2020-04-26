c89c22ae533e2e0bc6b14dea472cc56514fdaaf2
Jennings Anderson <jenningsanderson@colorado.edu>
2020-04-26 14:44:34 -0600

    well, it sort of works... it does break the API

--- docs/page.js
+++ docs/page.js
@@ -1,23 +1,29 @@
-
-
 function buildBeautifulListItemLink(object){
 	var liElem = document.createElement('li')
 
 	var displayName = object.name;
+	
+	//take out html
 	if (displayName.indexOf('.html') > 0){
 		displayName = object.name.substring(0,object.name.indexOf('.html'))
 	}
 
+    //replace dashes and underscores with spaces
 	displayName = displayName.replace(/_/g, " ");
 	displayName = displayName.replace(/-/g, " ");
+
+	//capitalize first letters
+	var parts = []
+	displayName.split(" ").forEach(function(t){
+		parts.push(t[0].toUpperCase() + t.substring(1,t.length))
+	})
+	displayName = parts.join(" ")
 
 	liElem.classList += 'analysis-item'
 
 	liElem.innerHTML = 
 
 		'<a href="'+object.name+'">' + displayName + '</a>'
-
-	
 	
 	return liElem
 }
